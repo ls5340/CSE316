c19e907270d6542209180d61382f8c18856180c0
David Huang <huangd1224@gmail.com>
2021-11-04 15:37:04 -0400

    HW4

--- Top5Lists_HW4/client/src/components/HomeScreen.js
+++ Top5Lists_HW4/client/src/components/HomeScreen.js
@@ -4,6 +4,7 @@ import ListCard from './ListCard.js'
 import { Fab, Typography } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add';
 import List from '@mui/material/List';
+import DeleteModal from './DeleteModal'
 /*
     This React component lists all the top5 lists in the UI.
     
@@ -36,6 +37,7 @@ const HomeScreen = () => {
     }
     return (
         <div id="top5-list-selector">
+            <DeleteModal />
             <div id="list-selector-heading">
             <Fab 
                 color="primary" 
